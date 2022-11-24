74d34e12bda77f62d1e016b977beae367a2d64ad
subhikshaParthiban <githublogin15@gmail.com>
2022-11-24 19:10:37 +0530

    adminsBackend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const claimsRouter = require('./src/routes/claims.route');
 const userRouter = require('./src/routes/users.route');
+const adminRouter = require('./src/routes/admins.route')
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,6 +23,7 @@ app.use('/claims', claimsRouter);
 
 app.use('/user', userRouter);
 
+app.use('/admin', adminRouter)
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
